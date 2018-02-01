61f41cdec77956f1b7b8f7a9c941855f2b2db428
zhangzhigang <zzg19901210@163.com>
2018-02-01 09:26:20 +0800

    Initial commit

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'rm-2ze0md47z82esntb8o.mysql.rds.aliyuncs.com';
+$cfg_dbname = 'dahwz';
+$cfg_dbuser = 'nmgd';
+$cfg_dbpwd = 'nmggd12#';
+$cfg_dbprefix = 'dah_';
+$cfg_db_language = 'utf8';
+
+
+?>
